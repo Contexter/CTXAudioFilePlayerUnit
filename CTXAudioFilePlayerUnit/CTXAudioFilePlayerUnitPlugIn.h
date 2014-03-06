69d5222a5771447cf63c80a3e2f00824fd3379af
Contexter <mail@benedikt-eickhoff.de>
2014-03-06 08:50:05 +0100

    Initial Commit

---
+++ CTXAudioFilePlayerUnit/CTXAudioFilePlayerUnitPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  CTXAudioFilePlayerUnitPlugIn.h
+//  CTXAudioFilePlayerUnit
+//
+//  Created by Benedikt Eickhoff on 06.03.14.
+//  Copyright (c) 2014 Benedikt Eickhoff. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface CTXAudioFilePlayerUnitPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
